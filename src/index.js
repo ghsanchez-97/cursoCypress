c9d627d1d0a3c9a8292a271e67dcabecb473a0f1
Guy Sánchez <anakin25@live.com>
2022-03-23 18:48:29 -0600

    :memo: Mini curso de JS, para entender bien a Cypress

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Declaración de variables
+let nombre = 'Guy'
+let ap='Sánchez'
+let edad = 24
+
+console.log(`Hola, mi nombre es ${nombre} ${ap} y tengo ${edad} años`)
+
+// Declaración de variables numéricos
+let a = 10
+let b = 20
+let s = a + b
+let r = a - b
+let m = a * b
+let d = a / b
+console.log(`La suma de ${a} + ${b} es ${s}`)
+console.log(`La resta de ${a} - ${b} es ${r}`)
+console.log(`La multiplicación de ${a} * ${b} es ${m}`)
+console.log(`La división de ${a} / ${b} es ${d}`)
+
+// Verificación de tipo de dato
+// let nomApe=`${nom} ${ape}`
+// console.log(`El nombre completo es ${nomApe}`)
+let nom='Guy'
+typeof nom
+let ape='Sánchez'
+typeof ape
+let ed=24
+typeof ed
