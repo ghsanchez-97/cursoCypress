1ef4046cb45b410eb2374be0611404cbba9a0f5c
Guy Sánchez <anakin25@live.com>
2022-06-07 20:24:18 -0600

    :white_check_mark: - ghsancehz - Guy Sánchez - Realizando test con selector xpath

--- cypress/integration/Seccion_1/Selectores.js
+++ cypress/integration/Seccion_1/Selectores.js
@@ -12,8 +12,16 @@ describe('Haciendo pruebas con los selectores', () => {
         cy.get('#userEmail').should('be.visible').should('be.enabled').should('be.empty').type('guy.sanchez97@hotmail.com')
         cy.wait(3500)
     })
-    it.only('Segundo ejemplo con selector de atributos', () => {
+    it('Segundo ejemplo con selector de atributos', () => {
         cy.get('[placeholder="Full Name"]').should('be.visible').should('be.enabled').should('be.empty').type('Maddison Hinata Sánchez Orh')
+        cy.wait(3500)
+    })
+    it.only('Tercer ejemplo con Xpath', () => {
+        cy.xpath('//*[@id="userName"]').should('be.visible').should('be.enabled').should('be.empty').type('Maddison Sánchez')
+        cy.wait(3500)
+        cy.xpath("//input[contains(@placeholder,'name@example.com')]").should('be.visible').should('be.enabled').should('be.empty').type('mhsanchez@live.com')
+        cy.wait(3500)
+        cy.xpath("//textarea[contains(@placeholder,'Current Address')]").should('be.visible').should('be.enabled').should('be.empty').type('Calle falsa 123')
         cy.wait(3500)
     })
 })
