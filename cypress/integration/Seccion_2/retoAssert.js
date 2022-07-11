c09819e5e4367caffe310dcc27a74c06d4f7eae4
Guy Sánchez <anakin25@live.com>
2022-07-11 14:22:35 -0600

    :white_check_mark: - ghsanchez - Guy Sánchez - Reto de los asserts concluido

--- cypress/integration/Seccion_2/retoAssert.js
+++ cypress/integration/Seccion_2/retoAssert.js
@@ -23,4 +23,59 @@ describe('Reto con los assets', () => {
             )
         })
     })
+    it('Realizando reto con asserts junto con las promesa, obteniendo el precio del vestido y se agrega al carrito de compra', () => {
+        cy.get('#block_top_menu').contains('Women').should('be.visible').click().then(() => {(
+            cy.wait(3500),
+            cy.get('.product-container').find('.product-image-container .product_img_link').eq(0).should('be.visible').click(50, 3, {force:true}).then(() => {
+                cy.wait(3500)
+                cy.get('#product_condition .editable').should('be.visible').then((e) => {
+                    let state = e.text()
+                    cy.log(state)
+                    if(state == 'New'){
+                        cy.log('El vestido es nuevo')
+                    }
+                })
+                cy.get('#our_price_display').should('be.visible').then((e) => {
+                    let price = e.text()
+                    price = price.slice(1,3)
+                    if(price > 30){
+                        cy.log('El vestido se sale de nuestro prosupuesto')
+                    }else{
+                        cy.log('El vestido se encuentra dentro del prosupuesto')
+                        cy.get('#add_to_cart .exclusive').should('be.visible').should('be.enabled').click()
+                        cy.wait(500)
+                    }
+                })
+            })
+            )
+        })
+    })
+    it('Realizando reto con asserts junto con las promesa, obteniendo el precio del vestido y se sale del prosupuesto', () => {
+        cy.get('#block_top_menu').contains('Women').should('be.visible').click().then(() => {(
+            cy.wait(3500),
+            cy.get('.product-container').find('.product-image-container .product_img_link').eq(3).should('be.visible').click(50, 3, {force:true}).then(() => {
+                cy.wait(3500)
+                cy.get('#product_condition .editable').should('be.visible').then((e) => {
+                    let state = e.text()
+                    cy.log(state)
+                    if(state == 'New'){
+                        cy.log('El vestido es nuevo')
+                    }
+                })
+                cy.get('#our_price_display').should('be.visible').then((e) => {
+                    let price = e.text()
+                    price = price.slice(1,3)
+                    if(price > 30){
+                        cy.log('El vestido se sale de nuestro prosupuesto')
+                        cy.xpath("(//a[contains(@href,'category=3&controller=category')])[2]").should('be.visible').click()
+                    }else{
+                        cy.log('El vestido se encuentra dentro del prosupuesto')
+                        cy.get('#add_to_cart .exclusive').should('be.visible').should('be.enabled').click()
+                        cy.wait(500)
+                    }
+                })
+            })
+            )
+        })
+    })
 })
